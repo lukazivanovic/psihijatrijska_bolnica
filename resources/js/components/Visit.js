4edaeb225b333162a91b30bb117bbacde89f2cef
alek101 <47416711+alek101@users.noreply.github.com>
2019-12-24 08:39:57 +0100

    save

--- resources/js/components/Visit.js
+++ resources/js/components/Visit.js
@@ -3,9 +3,42 @@ import React,{Component} from 'react';
 
 export default class Visit extends Component
 {
+    constructor(props)
+    {
+        super(props);
+    }
 
+    render()
+    {
+        return(
+            <div className="r_visit_form">
+                <div className="flexRowRight">
+                    <button>X</button>
+                </div>
 
-    render(
+                <div className="vueFormaVisit">
+                    <div className="donje">
+                        <div className="flexRow">
+                            <label>
+                                <select >
+                                    <option value="1">Prva Poseta</option>
+                                    <option value="0">Kontrolna Poseta</option>
+                                </select>
+                            </label> 
+                        </div>
+                    
+                    <label >Dijagnoza <textarea id="dijagnoza" cols="30" rows="10" ></textarea></label>
+                    <label >Terapija <textarea id="terapija" cols="30" rows="10" ></textarea></label>
         
-    )
+                        <div className="parentUpis">
+                                    
+                        </div>
+        
+                    <button className="submit" >Posalji u bazu</button>
+                </div>
+        
+            </div>
+        )
+    }
+        
 }
