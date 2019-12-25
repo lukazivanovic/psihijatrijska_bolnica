cb09fc78d10186df97ba50e3036770dec720a447
crnobrnjaLuka <47751680+crnobrnjaLuka@users.noreply.github.com>
2019-12-25 09:01:50 +0100

    Update API.js

--- resources/js/components/API.js
+++ resources/js/components/API.js
@@ -1,145 +1,154 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class API extends Component
-{
-    constructor()
-    {
+class API extends Component {
+    constructor() {
         super();
-        this.state={
-            zone:null,
-            datum:null,
-            time:null,
-            timeInterval:null,
-            name:null,
-            temp:null,
-            pressure:null,
-            humidity:null,
-            weatherInterval:null,
-            fetTime:"Europe/Belgrade",
-            fetWet:"Belgrade,rs",
+        this.state = {
+            zone: null,
+            datum: null,
+            time: null,
+            timeInterval: null,
+            name: null,
+            temp: null,
+            pressure: null,
+            humidity: null,
+            weatherInterval: null,
+            region: "Europe",
+            city: "Belgrade"
         }
 
-        this.pozoviTime=this.pozoviTime.bind(this);
-        this.continueCallTime=this.continueCallTime.bind(this);
-        this.pozoviWeather=this.pozoviWeather.bind(this);
-        this.continiueCallWeather=this.continiueCallWeather.bind(this);
-        this.setFetch=this.setFetch.bind(this);
+        this.pozoviTime = this.pozoviTime.bind(this);
+        this.pozoviWeather = this.pozoviWeather.bind(this);
+        this.rukujSelektorom = this.rukujSelektorom.bind(this)
     }
 
-    pozoviTime()
-    {
-        let opcije={
-            "method": "GET",
-        }
+    pozoviTime() {
+        fetch("http://worldtimeapi.org/api/timezone/" + this.state.region + "/" + this.state.city)
+            .then(rsp => rsp.json())
+            .then(response => {
+                // console.log(response);
+                this.setState({
+                    zone: response.timezone,
+                    datum: pf.dateToSerbianFormat(response.datetime.substring(0, 10)),
+                    time: response.datetime.substring(11, 16), //11,19 za sekunde
+                })
 
-        fetch("http://worldtimeapi.org/api/timezone/"+this.state.fetTime, opcije)
-        .then(rsp=>rsp.json())
-        .then(response => {
-            // console.log(response);
-            this.setState({
-                zone:response.timezone,
-                datum:pf.dateToSerbianFormat(response.datetime.substring(0,10)),
-                time:response.datetime.substring(11,16), //11,19 za sekunde
             })
-            
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }
-
-    continueCallTime()
-    {
-        let that=this;
-        that.setState({timeInterval:setInterval(that.pozoviTime,5000)});
-    }
-
-    pozoviWeather()
-    {
-        let opcije={
-            "method": "GET",
-        }
-
-        fetch("http://api.openweathermap.org/data/2.5/weather?q="+this.state.fetWet+"&APPID=17f779dd77988c24551bd27df2f061ae", opcije)
-        .then(rsp=>rsp.json())
-        .then(response => {
-            // console.log(response);
-            this.setState({
-                temp:(response.main.temp-272.15).toFixed(0),
-                name:response.name,
-                pressure:response.main.pressure,
-                humidity:response.main.humidity,
+            .catch(err => {
+                console.log(err);
             });
-            
-        })
-        .catch(err => {
-            console.log(err);
-        });
     }
 
-    continiueCallWeather()
-        {
-            let that=this;
-            that.setState({weatherInterval:setInterval(that.pozoviWeather,5000)});
-        }
+    pozoviWeather() {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=" + this.state.city + "&APPID=b214a5363d0244cc7a18d2586dc0314c")
+            .then(rsp => rsp.json())
+            .then(response => {
+                // console.log(response);
+                this.setState({
+                    temp: (response.main.temp - 272.15).toFixed(0),
+                    name: response.name,
+                    pressure: response.main.pressure,
+                    humidity: response.main.humidity,
+                });
+
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
 
-    componentDidMount()
-    {
+    componentDidMount() {
         this.pozoviTime();
-        this.continueCallTime();
         this.pozoviWeather();
-        this.continiueCallWeather();
+        this.timer = setInterval(() => {
+            this.pozoviTime();
+            this.pozoviWeather()
+        },
+            20000)
     }
 
-    componentWillUnmount()
-    {
-        let that=this;
-        clearInterval(that.state.timeInterval);
-        clearInterval(that.state.weatherInterval);
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.city !== this.state.city) {
+            console.log(this.state, prevState)
+            this.pozoviTime()
+            this.pozoviWeather()
+        }
     }
 
-    setFetch()
-    {
-        let that=this;
-        clearInterval(that.state.timeInterval);
-        clearInterval(that.state.weatherInterval);
-        let nizOdSelektora=document.querySelector('#selectorGrad').value.split('/');
-        that.setState({
-            fetTime:nizOdSelektora[0]+"/"+nizOdSelektora[1],
-            fetWet:nizOdSelektora[1]+","+nizOdSelektora[2],
-        });
-        that.pozoviTime();
-        that.continueCallTime();
-        that.pozoviWeather();
-        that.continiueCallWeather();
+    componentWillUnmount() {
+        clearInterval(this.timer);
+
     }
 
-    render()
-    {
+    rukujSelektorom(event) {
+        let city = event.target.value
+        let region = city === "Tokyo" ? "Asia" : "Europe"
+        this.setState({
+            region: region,
+            city: city
+        })
+    }
+
+    promeniSliku(city) {
+        switch (city) {
+            case "Belgrade":
+                return <img src="/images/city.png" alt="city"></img>;
+            case "Paris":
+                return <img src="/images/paris.png" alt="city"></img>;
+            case "Tokyo":
+                return <img src="/images/tokyo.png" alt="city"></img>;
+            case "London":
+                return <img src="/images/London.png" alt="city"></img>;
+            default:
+                return <img src="/images/city.png" alt="city"></img>;
+        }
+    }
+
+    render() {
         //destructuring
-        const {zone,datum,time,name,temp,pressure,humidity}=this.state;
-        return(
-                    <div className="reactApi">
-                        <select  id="selectorGrad" onChange={this.setFetch}>
-                            <option value="Europe/Belgrade/rs">Beograd</option>
-                            <option value="Europe/Paris/fr">Paris</option>
-                            <option value="Europe/London/uk">London</option>
-                            <option value="Asia/Tokyo/jp">Tokio</option>
-                        </select>
-                    <div>
-                        Zona: <p>{ zone }</p>
-                        Datum: <p>{ datum }</p>
-                        Vreme: <p>{ time }</p>
+        const { zone, datum, time, name, temp, pressure, humidity } = this.state;
+        return (
+            <div className="reactApi">
+                <div className="reactCity">
+                    <p className="city">{this.promeniSliku(this.state.city)} {name}</p>
+                    <p className="selectCity">Izaberi grad</p>
+                    <select id="selectorGrad" onChange={this.rukujSelektorom}>
+                        <option value="Belgrade">Beograd</option>
+                        <option value="Paris">Paris</option>
+                        <option value="London">London</option>
+                        <option value="Tokyo">Tokio</option>
+                    </select>
+                </div>
+                <div className="reactDate">
+                    <div className="dateItems">
+                        <img src="/images/time-zone.png" alt="time-zone"></img>
+                        <span>{zone}</span>
                     </div>
-
-                    <div>
-                        Grad: <p>{ name }</p>
-                        Temperatura: <p>{ temp } C</p> 
-                        Pritisak: <p>{ pressure }</p>
-                        Vlaznost vazduha: <p>{ humidity } %</p>
+                    <div className="dateItems">
+                        <img src="/images/calendar.png" alt="calendar"></img>
+                        <span>{datum}</span>
                     </div>
+                    <div className="dateItems">
+                        <img src="/images/clock.png" alt="clock"></img>
+                        <span>{time}</span>
+                    </div>
+                </div>
+                <div className="reactWeather">
+                    <div className="temperature">
+                        <img src="/images/thermometer.png" alt="temperature"></img>
+                        <span>{temp} C</span>
+                    </div>
+                    <div className="temperature">
+                        <img src="/images/pressure.png" alt="pressure"></img>
+                        <span>{pressure} mB</span>
+                    </div>
+                    <div className="temperature">
+                        <img src="/images/humidity.png" alt="humidity"></img>
+                        <span>{humidity} %</span>
+                    </div>
+                </div>
             </div>
         );
     }
