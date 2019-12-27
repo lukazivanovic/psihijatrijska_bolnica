8a24b9871db8c597aec6f6620754a533d4c5b99d
crnobrnjaLuka <47751680+crnobrnjaLuka@users.noreply.github.com>
2019-12-27 22:26:49 +0100

    css
    menjao - > kartoniStranica.js karton_osnovni_podaci.js karton.visit.js karton_visit_treatmen.js detalji.css

--- resources/js/components/Karton_visit.js
+++ resources/js/components/Karton_visit.js
@@ -1,52 +1,45 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Karton_visit_treatmant from './Karton_visit_treatmant';
 
-export default class Karton_visit extends Component
-{
-    constructor(props)
-    {
+export default class Karton_visit extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            show_div:false,
-            show_edit:false,
-            dijagnoza:this.props.data.dijagnoza,
-            terapija:this.props.data.terapija,
-            odgovor:null,
+        this.state = {
+            show_div: false,
+            show_edit: false,
+            dijagnoza: this.props.data.dijagnoza,
+            terapija: this.props.data.terapija,
+            odgovor: null,
         }
 
-        this.otkrijDetalje=this.otkrijDetalje.bind(this);
-        this.izmeni=this.izmeni.bind(this);
-        this.dijagnozaArea=React.createRef();
-        this.setDijagnoza=this.setDijagnoza.bind(this);
-        this.terapijaArea=React.createRef();
-        this.setTerapija=this.setTerapija.bind(this);
-        this.sendData=this.sendData.bind(this);
-        this.callLaravel=this.callLaravel.bind(this);
+        this.otkrijDetalje = this.otkrijDetalje.bind(this);
+        this.izmeni = this.izmeni.bind(this);
+        this.dijagnozaArea = React.createRef();
+        this.setDijagnoza = this.setDijagnoza.bind(this);
+        this.terapijaArea = React.createRef();
+        this.setTerapija = this.setTerapija.bind(this);
+        this.sendData = this.sendData.bind(this);
+        this.callLaravel = this.callLaravel.bind(this);
 
     }
 
-    otkrijDetalje()
-    {
-        this.setState({show_div:!this.state.show_div});
+    otkrijDetalje() {
+        this.setState({ show_div: !this.state.show_div });
     }
 
-    izmeni()
-    {
-        this.setState({show_edit:!this.state.show_edit});
+    izmeni() {
+        this.setState({ show_edit: !this.state.show_edit });
     }
 
-    setDijagnoza()
-    {
-        this.setState({dijagnoza:this.dijagnozaArea.current.value});
+    setDijagnoza() {
+        this.setState({ dijagnoza: this.dijagnozaArea.current.value });
     }
 
-    setTerapija()
-    {
-        this.setState({terapija:this.terapijaArea.current.value});
+    setTerapija() {
+        this.setState({ terapija: this.terapijaArea.current.value });
     }
 
-    callLaravel()
-    {
+    callLaravel() {
         this.props.callLaravel();
     }
 
@@ -55,17 +48,16 @@ export default class Karton_visit extends Component
     //     this.setState({odgovor:txt});
     // }
 
-    sendData()
-    {
-        let niz={
-            prva_poseta:this.props.data.prva_poseta,
-            tracers:this.props.id,
-            dijagnoza:this.state.dijagnoza || this.props.data.dijagnoza,
-            terapija:this.state.terapija || this.props.data.terapija,
-            json:true, //da kazemo serveru da vrati json odgovor
+    sendData() {
+        let niz = {
+            prva_poseta: this.props.data.prva_poseta,
+            tracers: this.props.id,
+            dijagnoza: this.state.dijagnoza || this.props.data.dijagnoza,
+            terapija: this.state.terapija || this.props.data.terapija,
+            json: true, //da kazemo serveru da vrati json odgovor
         }
 
-        let opcije={
+        let opcije = {
             method: "POST",
             body: JSON.stringify(niz),
             headers: {
@@ -75,36 +67,33 @@ export default class Karton_visit extends Component
         }
 
         // console.log('niz',niz,'token', document.querySelector('input[name="_token"]').value);
-        
-        fetch('/lekar/updateVisit',opcije)
-            .then(resp=>resp.json())
-            .then(txt=>
-                {
-                    this.setState({odgovor:txt}); 
-                    this.izmeni();
-                    this.callLaravel();
-                });
+
+        fetch('/lekar/updateVisit', opcije)
+            .then(resp => resp.json())
+            .then(txt => {
+                this.setState({ odgovor: txt });
+                this.izmeni();
+                this.callLaravel();
+            });
     }
 
-    render()
-    {
-        let treatmants=this.props.data.lekovi.map(lek=><Karton_visit_treatmant key={lek.ind_tracer} id={lek.ind_tracer} 
-            data={lek} show_div={this.state.show_div} callLaravel={this.callLaravel}/>)
+    render() {
+        let treatmants = this.props.data.lekovi.map(lek => <Karton_visit_treatmant key={lek.ind_tracer} id={lek.ind_tracer}
+            data={lek} show_div={this.state.show_div} callLaravel={this.callLaravel} />)
 
-        if(this.state.show_div && !this.state.show_edit)
-        {
-            return(
+        if (this.state.show_div && !this.state.show_edit) {
+            return (
                 <div className="r_visit_screen">
 
                     <div className="oneVisit">
                         <div className="flexRowRight"><button onClick={this.otkrijDetalje}>X</button></div>
-                        
-                        <div className="flexRowES"> 
+
+                        <div className="flexRowES">
                             <div> Datum: {pf.dateToSerbianFormat(this.props.data.datum)} </div>
-                            <div> Tip: {(this.props.data.prva_poseta)? "Prva poseta":"Kontrolna poseta"} </div>
+                            <div> Tip: {(this.props.data.prva_poseta) ? "Prva poseta" : "Kontrolna poseta"} </div>
                             <div> Lekar: {this.props.data.id_lekar} </div>
                         </div>
-        
+
                         <div>
                             <h4 className="r_naslov_c">Dijagnoza: </h4>
                             {this.props.data.dijagnoza}
@@ -114,68 +103,69 @@ export default class Karton_visit extends Component
                             {this.props.data.terapija}
                         </div>
                         <div className="flexRowRight"><button onClick={this.izmeni}>Edit</button></div>
-                        
-                            {treatmants}
+
+                        {treatmants}
                         <div className="r_error">{this.state.odgovor}</div>
                     </div>
-                    
+
                 </div>
-                
+
             )
         }
-        if(this.state.show_div && this.state.show_edit)
-        {
-            return(
+        if (this.state.show_div && this.state.show_edit) {
+            return (
                 <div className="r_visit_screen">
-                    
-                <div className="oneVisit r_oneVisit">
-                    <div className="flexRowRight"><button onClick={this.otkrijDetalje}>X</button></div>
-                    <div className="flexRowES"> 
-                        <div> Datum: {pf.dateToSerbianFormat(this.props.data.datum)} </div>
-                        <div> Tip: {(this.props.data.prva_poseta)? "Prva poseta":"Kontrolna poseta"} </div>
-                        <div> Lekar: {this.props.data.id_lekar} </div>
-                    </div>
-    
-                    <div>
-                        
-                        
-                        <label>
-                            <p>Dijagnoza:</p>
-                            
-                            <textarea defaultValue={this.props.data.dijagnoza} ref={this.dijagnozaArea} onChange={this.setDijagnoza}/>
-                        </label>
-                    </div>
-                    <div>
-                        
-                        <label>
-                            <p>Terapija:</p>
-                            
-                            <textarea defaultValue={this.props.data.terapija} ref={this.terapijaArea} onChange={this.setTerapija}/>
-                        </label>
-        
-                    </div>
-                    <button onClick={this.izmeni}>Odustani</button>
-                    <button onClick={this.sendData}>Posalji</button>
-                    <div className="r_error">{this.state.odgovor}</div>
+
+                    <div className="oneVisit r_oneVisit">
+                        <div className="flexRowRight"><button onClick={this.otkrijDetalje}>X</button></div>
+                        <div className="flexRowES">
+                            <div> Datum: {pf.dateToSerbianFormat(this.props.data.datum)} </div>
+                            <div> Tip: {(this.props.data.prva_poseta) ? "Prva poseta" : "Kontrolna poseta"} </div>
+                            <div> Lekarrrrrrrrrrr: {this.props.data.id_lekar} </div>
+                        </div>
+
+                        <div>
+
+
+                            <label>
+                                <p>Dijagnoza:</p>
+
+                                <textarea defaultValue={this.props.data.dijagnoza} ref={this.dijagnozaArea} onChange={this.setDijagnoza} />
+                            </label>
+                        </div>
+                        <div>
+
+                            <label>
+                                <p>Terapija:</p>
+
+                                <textarea defaultValue={this.props.data.terapija} ref={this.terapijaArea} onChange={this.setTerapija} />
+                            </label>
+
+                        </div>
+                        <button onClick={this.izmeni}>Odustani</button>
+                        <button onClick={this.sendData}>Posalji</button>
+                        <div className="r_error">{this.state.odgovor}</div>
                         {treatmants}
+                    </div>
                 </div>
-            </div>
-                
-                
+
+
             )
         }
-        return(
+        return (
             <div className="r_visit" onClick={this.otkrijDetalje}>
-                <div className="flexRowES"> 
+                <div className="flexRowES">
                     <div> Datum: {pf.dateToSerbianFormat(this.props.data.datum)} </div>
-                    <div> Tip: {(this.props.data.prva_poseta)? "Prva poseta":"Kontrolna poseta"} </div>
+                    <div> Tip: {(this.props.data.prva_poseta) ? "Prva poseta" : "Kontrolna poseta"} </div>
                     <div> Lekar: {this.props.data.id_lekar} </div>
+                    <div className="r_naslov">Dijagnoza: {this.props.data.dijagnoza.slice(0, 96)}</div>
+                    <div className="r_naslov">Terapija: {this.props.data.terapija.slice(0, 96)}</div>
+                    {treatmants}
                 </div>
-                
-                <div className="flexRow"><div><span className="r_naslov">Dijagnoza: </span> {this.props.data.dijagnoza.slice(0,96)}</div></div>
-                <div className="flexRow"><div><span className="r_naslov">Terapija: </span> {this.props.data.terapija.slice(0,96)}</div></div>
-                
-                {treatmants}
+
+
+
+
             </div>
         )
     }
--- resources/js/components/Karton_visit_treatmant.js
+++ resources/js/components/Karton_visit_treatmant.js
@@ -1,63 +1,55 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-export default class Karton_visit_treatmant extends Component
-{
-    constructor(props)
-    {
+export default class Karton_visit_treatmant extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            sifra_bolesti:this.props.data.sifra_bolest,
-            sifra_leka:this.props.data.sifra_lek,
-            lek_prepisana_kol:this.props.data.lek_prepisana_kol,
-            show_edit_treatment:false,
+        this.state = {
+            sifra_bolesti: this.props.data.sifra_bolest,
+            sifra_leka: this.props.data.sifra_lek,
+            lek_prepisana_kol: this.props.data.lek_prepisana_kol,
+            show_edit_treatment: false,
         }
 
-        this.callLaravel=this.callLaravel.bind(this);
-        this.izmeni=this.izmeni.bind(this);
-        this.sb=React.createRef();
-        this.sl=React.createRef();
-        this.kl=React.createRef();
-        this.setSB=this.setSB.bind(this);
-        this.setSL=this.setSL.bind(this);
-        this.setKL=this.setKL.bind(this);
-        this.sendData=this.sendData.bind(this);
+        this.callLaravel = this.callLaravel.bind(this);
+        this.izmeni = this.izmeni.bind(this);
+        this.sb = React.createRef();
+        this.sl = React.createRef();
+        this.kl = React.createRef();
+        this.setSB = this.setSB.bind(this);
+        this.setSL = this.setSL.bind(this);
+        this.setKL = this.setKL.bind(this);
+        this.sendData = this.sendData.bind(this);
     }
 
-    callLaravel()
-    {
+    callLaravel() {
         this.props.callLaravel();
     }
 
-    izmeni()
-    {
-        this.setState({show_edit_treatment:!this.state.show_edit_treatment});
+    izmeni() {
+        this.setState({ show_edit_treatment: !this.state.show_edit_treatment });
     }
 
-    setSB()
-    {
-        this.setState({sifra_bolesti:this.sb.current.value});
+    setSB() {
+        this.setState({ sifra_bolesti: this.sb.current.value });
     }
 
-    setSL()
-    {
-        this.setState({sifra_leka:this.sl.current.value});
+    setSL() {
+        this.setState({ sifra_leka: this.sl.current.value });
     }
 
-    setKL()
-    {
-        this.setState({lek_prepisana_kol:this.kl.current.value});
+    setKL() {
+        this.setState({ lek_prepisana_kol: this.kl.current.value });
     }
 
-    sendData()
-    {
-        let niz={
-            id:this.props.id,
-            sifra_bolesti:this.state.sifra_bolesti || this.props.data.sifra_bolest,
-            sifra_leka:this.state.sifra_leka || this.props.data.sifra_lek,
-            lek_prepisana_kol:this.state.lek_prepisana_kol || this.props.data.lek_prepisana_kol,
+    sendData() {
+        let niz = {
+            id: this.props.id,
+            sifra_bolesti: this.state.sifra_bolesti || this.props.data.sifra_bolest,
+            sifra_leka: this.state.sifra_leka || this.props.data.sifra_lek,
+            lek_prepisana_kol: this.state.lek_prepisana_kol || this.props.data.lek_prepisana_kol,
         }
 
-        let opcije={
+        let opcije = {
             method: "POST",
             body: JSON.stringify(niz),
             headers: {
@@ -67,66 +59,63 @@ export default class Karton_visit_treatmant extends Component
         }
 
         // console.log('niz',niz,'token', document.querySelector('input[name="_token"]').value);
-        
-        fetch('/lekar/updateTreatmantReact',opcije)
-            .then(resp=>resp.json())
-            .then(txt=>
-                {
-                  alert(txt);
-                  this.izmeni();
-                  this.callLaravel();
-                }).catch(err => {
-                    console.log(err);
-                });;
+
+        fetch('/lekar/updateTreatmantReact', opcije)
+            .then(resp => resp.json())
+            .then(txt => {
+                alert(txt);
+                this.izmeni();
+                this.callLaravel();
+            }).catch(err => {
+                console.log(err);
+            });;
     }
 
-    render()
-    {
-        if(this.props.show_div && !this.state.show_edit_treatment)
-        {
-            return(
-              <div className="r_treatmant r_space_beetwen">
-                    
-                        <div>Ime Bolesti: {this.props.data.name_bolest}</div>
-                        <div>Ime Leka: {this.props.data.name_lek}</div>
-                        <div>Kolicina: {this.props.data.lek_prepisana_kol}</div>
-                        <button onClick={this.izmeni}>Edit</button>
-                    
-                </div>  
-            )   
-        }
+    render() {
+        if (this.props.show_div && !this.state.show_edit_treatment) {
+            return (
+                <div className="r_treatmant r_space_beetwen">
 
-        if(this.props.show_div && this.state.show_edit_treatment)
-        {
-            return(
-              <div className="r_treatmant r_space_beetwen">
-                    
-                        <label>
-                            Sifra Bolesti: <input type="text" defaultValue={this.props.data.sifra_bolest} ref={this.sb} onChange={this.setSB}/>
-                        </label>
-                        <label>
-                            Sifra Leka: <input type="text" defaultValue={this.props.data.sifra_lek} ref={this.sl} onChange={this.setSL}/>
-                        </label>
-                        <label>
-                            Kolicina:
-                            <input type="number" defaultValue={this.props.data.lek_prepisana_kol} ref={this.kl} onChange={this.setKL}/>
-                        </label>
-                        
-                        <button onClick={this.izmeni}>Odustani</button>
-                        <button onClick={this.sendData}>Posalji</button>
-                    
-                </div>  
-            )   
-        }
-
-        return(
-            <div className="r_treatmant">
-                
                     <div>Ime Bolesti: {this.props.data.name_bolest}</div>
-                    <div>Lek: {this.props.data.name_lek}</div>
+                    <div>Ime Leka: {this.props.data.name_lek}</div>
                     <div>Kolicina: {this.props.data.lek_prepisana_kol}</div>
-                
+                    <button onClick={this.izmeni}>Edit</button>
+
+                </div>
+            )
+        }
+
+        if (this.props.show_div && this.state.show_edit_treatment) {
+            return (
+                <div className="r_treatmant r_space_beetwen">
+
+                    <label>
+                        Sifra Bolesti: <input type="text" defaultValue={this.props.data.sifra_bolest} ref={this.sb} onChange={this.setSB} />
+                    </label>
+                    <label>
+                        Sifra Leka: <input type="text" defaultValue={this.props.data.sifra_lek} ref={this.sl} onChange={this.setSL} />
+                    </label>
+                    <label>
+                        Kolicina:
+                        <input type="number" defaultValue={this.props.data.lek_prepisana_kol} ref={this.kl} onChange={this.setKL} />
+                    </label>
+
+                    <button onClick={this.izmeni}>Odustani</button>
+                    <button onClick={this.sendData}>Posalji</button>
+
+                </div>
+            )
+        }
+
+        return (
+
+            <div className="r_treatmant">
+                <div>Ime Bolesti: {this.props.data.name_bolest}</div>
+                <div>Lek: {this.props.data.name_lek}</div>
+                <div>Kolicina: {this.props.data.lek_prepisana_kol}</div>
             </div>
+
+
         )
     }
 }
