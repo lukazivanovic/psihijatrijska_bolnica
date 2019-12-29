24cf77cfd8a9346dc3326883ffb65d4b2dbe1c0e
alek101 <47416711+alek101@users.noreply.github.com>
2019-12-29 12:06:13 +0100

    Verzija 3.6.2
    Dodati error-ovi i u lekar folderu.

--- resources/views/lekar/allPatients.blade.php
+++ resources/views/lekar/allPatients.blade.php
@@ -5,13 +5,17 @@
 @auth
     <div class="margin_20">
         <label for="filter">
-            <input type="text" id="filter" class='filter' style="background-image: url('/images/search.png')" placeholder="Pretraži...">
+            <input type="text" id="filter" class='filter' placeholder="Pretraži...">
         </label>
     </div>
 @endauth
 
 <div class="patient-list">
     <h1>Spisak pacijenata</h1>
+
+    @foreach ($errors->all() as $error)
+        <p class="r_error">{{ $error }}</p>
+    @endforeach
 
     <table>
         <head>
