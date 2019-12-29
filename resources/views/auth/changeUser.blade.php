c99c43004c71164124ed6b1730403d9dbff3f153
crnobrnjaLuka <47751680+crnobrnjaLuka@users.noreply.github.com>
2019-12-29 22:33:27 +0100

    css
    css

--- resources/views/auth/changeUser.blade.php
+++ resources/views/auth/changeUser.blade.php
@@ -1,8 +1,74 @@
-@extends('layouts.app')
+@extends('layouts.index')
 
 @section('content')
+<link rel="stylesheet" href="{{ url('/css/osobljeStyle.css') }}">
 
-<div class="row  text-center">
+<div class="margin_20 flexColumn">
+    <div class="bolestiNaslov">
+        <h1>
+            Izmeni podatke
+        </h1>
+    </div>
+</div>
+
+
+
+<div class="promenaLoz">
+    <form method="POST" action="/madeChangeUser">
+        @csrf
+
+        <input type="text" name="id" value="{{ Auth::user()->id }}" requered hidden>
+
+        <div class="row  text-center">
+            <div class="col-md-12">
+                @foreach ($errors->all() as $error)
+                <p class="text-danger">{{ $error }}</p>
+                @endforeach
+            </div>
+        </div>
+
+
+        <div class="okvir1">
+            <div class="okvir11">
+                <label for="name" class="col-md-4 col-form-label text-md-right">Username</label>
+            </div>
+
+
+            <div class="okvir12  @error('current_password') {{'has-error'}} @enderror">
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
+                @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+        </div>
+
+
+        <div class="okvir1">
+            <div class="okvir11">
+                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+            </div>
+
+            <div class="okvir12  @error('new_password') {{'has-error'}} @enderror">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+        </div>
+        <div class="divPrijava">
+            <button type="submit" class="dugmePrijava" style="background-image: url('/images/check_32.png')">
+            </button>
+        </div>
+
+
+    </form>
+</div>
+</div>
+<!-- <div class="row  text-center">
     <div class="col-md-12">
         @foreach ($errors->all() as $error)
         <p class="text-danger">{{ $error }}</p>
@@ -77,5 +143,5 @@
         </div>
     </div>
 </div>
-</div>
+</div> -->
 @endsection
