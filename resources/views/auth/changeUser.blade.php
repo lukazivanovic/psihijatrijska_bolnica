f8516863eaf157ffe8b9f46f85baace67a8c4992
alexvilus <45175683+alexvilus@users.noreply.github.com>
2019-12-29 22:36:45 +0100

    ...

--- resources/views/auth/changeUser.blade.php
+++ resources/views/auth/changeUser.blade.php
@@ -1,6 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.index')
 
 @section('content')
+<link rel="stylesheet" href="{{ url('/css/osobljeStyle.css') }}">
+
 
 <div class="row  text-center">
     <div class="col-md-12">
@@ -9,73 +11,83 @@
         @endforeach
     </div>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card ">
-                    <div class="card-header">Izmeni</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="/madeChangeUser">
-                            @csrf
-
-
-
-                            <input type="text" name="id" value="{{ Auth::user()->id }}" requered hidden>
-
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Username</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
-
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            {{-- <div class="form-group row">
-                            <label for="phone" class="col-md-4 col-form-label text-md-right">Telefon</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ Auth::user()->phone }}" required >
-
-                            @error('phone')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                    </div>
-                </div> --}}
-
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            Izmeni
-                        </button>
-                    </div>
+    <div class="margin_20 flexColumn">
+        <div class="bolestiNaslov">
+            <h1>
+                <div>
+                    {{ __('Registracija') }}
                 </div>
-                </form>
-            </div>
+            </h1>
         </div>
     </div>
-</div>
+
+
+    <div class="promenaLoz">
+        <form method="POST" action="/madeChangeUser">
+            @csrf
+
+
+
+            <input type="text" name="id" value="{{ Auth::user()->id }}" requered hidden>
+
+            <div class="form-group row">
+                <label for="name" class="col-md-4 col-form-label text-md-right">
+                    Username
+                </label>
+
+                <div class="col-md-6">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
+
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="email" class="col-md-4 col-form-label text-md-right">  
+                    Email
+                </label>
+
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            {{-- 
+            <div class="form-group row">
+                <label for="phone" class="col-md-4 col-form-label text-md-right">
+                    Telefon
+                </label>
+            </div>
+
+            <div class="col-md-6">
+                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ Auth::user()->phone }}" required >
+
+                @error('phone')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            --}}
+
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">
+                        Izmeni
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
