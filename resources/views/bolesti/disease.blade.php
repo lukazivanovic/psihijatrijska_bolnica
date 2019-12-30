37131d5eb2347745336e05b24d3a268cc4f81f34
crnobrnjaLuka <47751680+crnobrnjaLuka@users.noreply.github.com>
2019-12-30 10:36:57 +0100

    css
    css

--- resources/views/bolesti/disease.blade.php
+++ resources/views/bolesti/disease.blade.php
@@ -11,42 +11,43 @@
         <h1>Oboljenja</h1>
     </div>
 
-    <label class="osobFilter" for="filter">
+    <label class="osobFilter leftMargin" for="filter">
         <input type="text" id="filter" class='kartonInput' style="background-image: url('/images/search.png')" placeholder="Pretraži...">
     </label>
+    <div class="tabelaOboljenja">
+
+
+        @if (Auth::user()->role==1)
+        <button class='linkDugme w100' data-link='/bolesti/create'>Dodaj novo oboljenje</button>
+        @endif
+
+
+        <table>
+            <tr>
+                <th>Šifra</th>
+                <th>Naziv</th>
+                @if (Auth::user()->role==1)
+                <th>Izmeni</th>
+                <!-- <th>Obrisi</th> -->
+                @endif
+            </tr>
+
+            @foreach ($diseases as $disease)
+            <tr>
+                <td>{{ $disease->sifra_bolest }}</td>
+                <td>{{ $disease->ime_bolest }}</td>
+                @if (Auth::user()->role==1)
+                <td><button class='linkDugme linkDugmeIzmeni' data-link='/bolesti/edit/{{ $disease->id }}' style="background-image: url('/images/pencil.png')"></button></td>
+                <!-- <td><button class='obrisi' data-link='/bolesti/destroy/{{ $disease->id }}'>Obrisi</button></td> -->
+                @endif
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
 @endauth
 @foreach ($errors->all() as $error)
 <p class="r_error">{{ $error }}</p>
 @endforeach
-<div class="tabelaOboljenja">
 
-
-    @if (Auth::user()->role==1)
-    <button class='linkDugme w100' data-link='/bolesti/create'>Dodaj novo oboljenje</button>
-    @endif
-
-
-    <table>
-        <tr>
-            <th>Šifra</th>
-            <th>Naziv</th>
-            @if (Auth::user()->role==1)
-            <th>Izmeni</th>
-            <!-- <th>Obrisi</th> -->
-            @endif
-        </tr>
-
-        @foreach ($diseases as $disease)
-        <tr>
-            <td>{{ $disease->sifra_bolest }}</td>
-            <td>{{ $disease->ime_bolest }}</td>
-            @if (Auth::user()->role==1)
-            <td><button class='linkDugme linkDugmeIzmeni' data-link='/bolesti/edit/{{ $disease->id }}' style="background-image: url('/images/pencil.png')"></button></td>
-            <!-- <td><button class='obrisi' data-link='/bolesti/destroy/{{ $disease->id }}'>Obrisi</button></td> -->
-            @endif
-        </tr>
-        @endforeach
-    </table>
-</div>
 @endsection
--- resources/views/osoblje/allPatients.blade.php
+++ resources/views/osoblje/allPatients.blade.php
@@ -14,57 +14,59 @@
     <p class="r_error">{{ $error }}</p>
     @endforeach
 
-    <label for="filter">
+    <label class="leftMargin" for="filter">
         <input type="text" id="filter" class='kartonInput' placeholder="Pretraži...">
     </label>
+
+    <div class="tabelaOboljenja">
+
+
+        <button class='linkDugme w100' data-link='/osoblje/create'>Dodaj Novog</button>
+
+        <table>
+
+            <head>
+                <th>Karton</th>
+                <th>Ime</th>
+                <th>Prezime</th>
+                <!-- <th>Datum Rodjenja</th> -->
+                <th>Pol</th>
+                <th>JMBG</th>
+                <th>Grad</th>
+                <th>Ulica</th>
+                <th>Broj</th>
+                <th>Doktor</th>
+                <th>Izmeni</th>
+                <!-- <th>Obrisi</th> -->
+            </head>
+
+            @foreach ($listaPacijenata as $pacijent)
+            @php
+            $date=explode('-',$pacijent->dat_rodjenja);
+            $date=array_reverse($date);
+            $pacijent->dat_rodjenja=implode('.',$date).".";
+            @endphp
+            <tr>
+                <td>K-{{ $pacijent->id }}</td>
+                <td>{{ $pacijent->ime }}</td>
+                <td>{{ $pacijent->prezime }}</td>
+                <!-- <td>{{ $pacijent->dat_rodjenja }}</td> -->
+                <td>{{ $pacijent->pol }}</td>
+                <td>{{ $pacijent->jmbg }}</td>
+                <td>{{ $pacijent->grad }}</td>
+                <td>{{ $pacijent->ulica }}</td>
+                <td>{{ $pacijent->broj }}</td>
+                <td>{{ $pacijent->name }}</td>
+                <td><button class='linkDugme linkDugmeIzmeni' data-link='/osoblje/edit/{{ $pacijent->id }}' style="background-image: url('/images/pencil.png')"></button></td>
+                <!-- <td><button class='obrisi' data-link='/osoblje/destroy/{{ $pacijent->id }}'>Ubij</button></td> -->
+            </tr>
+
+            @endforeach
+
+        </table>
+    </div>
 </div>
 @endauth
-<div class="tabelaOboljenja">
 
-
-    <button class='linkDugme w100' data-link='/osoblje/create'>Dodaj Novog</button>
-
-    <table>
-
-        <head>
-            <th>Karton</th>
-            <th>Ime</th>
-            <th>Prezime</th>
-            <!-- <th>Datum Rodjenja</th> -->
-            <th>Pol</th>
-            <th>JMBG</th>
-            <th>Grad</th>
-            <th>Ulica</th>
-            <th>Broj</th>
-            <th>Doktor</th>
-            <th>Izmeni</th>
-            <!-- <th>Obrisi</th> -->
-        </head>
-
-        @foreach ($listaPacijenata as $pacijent)
-        @php
-        $date=explode('-',$pacijent->dat_rodjenja);
-        $date=array_reverse($date);
-        $pacijent->dat_rodjenja=implode('.',$date).".";
-        @endphp
-        <tr>
-            <td>K-{{ $pacijent->id }}</td>
-            <td>{{ $pacijent->ime }}</td>
-            <td>{{ $pacijent->prezime }}</td>
-            <!-- <td>{{ $pacijent->dat_rodjenja }}</td> -->
-            <td>{{ $pacijent->pol }}</td>
-            <td>{{ $pacijent->jmbg }}</td>
-            <td>{{ $pacijent->grad }}</td>
-            <td>{{ $pacijent->ulica }}</td>
-            <td>{{ $pacijent->broj }}</td>
-            <td>{{ $pacijent->name }}</td>
-            <td><button class='linkDugme linkDugmeIzmeni' data-link='/osoblje/edit/{{ $pacijent->id }}' style="background-image: url('/images/pencil.png')"></button></td>
-            <!-- <td><button class='obrisi' data-link='/osoblje/destroy/{{ $pacijent->id }}'>Ubij</button></td> -->
-        </tr>
-
-        @endforeach
-
-    </table>
-</div>
 
 @endsection
